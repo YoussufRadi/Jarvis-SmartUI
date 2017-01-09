e10c252acdad8186a913bc54f51ca0a7704126ee
Youssuf Radi <y.sameh1995@gmail.com>
2017-01-09 05:24:50 +0200

    Settings added to account

--- app/src/main/java/com/winterproject/youssufradi/jarvis_smartui/MainActivity.java
+++ app/src/main/java/com/winterproject/youssufradi/jarvis_smartui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.winterproject.youssufradi.jarvis_smartui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -68,7 +69,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            startActivity(new Intent(MainActivity.this, Setting.class));
         }
 
         return super.onOptionsItemSelected(item);
